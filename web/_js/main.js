ce4877e97b436bf269b8142670d544783e4d8439
Hans5958 <hans5958@outlook.com>
2022-05-10 12:13:05 +0700

    Add shimmed atlas from current remaster
    https://github.com/placeAtlas/atlas/commit/275ac226843c1635d7f863bbee0cfcf9f1f446ba

---
+++ web/_js/main.js
@@ -0,0 +1,428 @@
+/*
+	========================================================================
+	The 2022 r/place Atlas
+
+	An atlas of Reddit's 2022 r/place, with information to each
+	artwork	of the canvas provided by the community.
+
+	Copyright (c) 2017 Roland Rytz <roland@draemm.li>
+	Copyright (c) 2022 Place Atlas contributors
+
+	Licensed under the GNU Affero General Public License Version 3
+	https://place-atlas.stefanocoding.me/license.txt
+	========================================================================
+*/
+
+const prodDomain = "place-atlas.stefanocoding.me"
+
+const innerContainer = document.getElementById("innerContainer")
+const container = document.getElementById("container")
+const canvas = document.getElementById("highlightCanvas")
+const context = canvas.getContext("2d")
+
+let zoom = 1
+
+if (window.devicePixelRatio) {
+	zoom = 1 / window.devicePixelRatio
+}
+
+const maxZoom = 128
+const minZoom = 0.1
+
+let zoomOrigin = [0, 0]
+let scaleZoomOrigin = [0, 0]
+
+let dragging = false
+let lastPosition = [0, 0]
+
+const viewportSize = [0, 0]
+
+function applyView() {
+
+	//console.log(zoomOrigin, scaleZoomOrigin)
+	//console.log(scaleZoomOrigin[0])
+
+	scaleZoomOrigin[0] = Math.max(-1000, Math.min(1000, scaleZoomOrigin[0]))
+	scaleZoomOrigin[1] = Math.max(-1000, Math.min(1000, scaleZoomOrigin[1]))
+
+	zoomOrigin = [scaleZoomOrigin[0] * zoom, scaleZoomOrigin[1] * zoom]
+
+	innerContainer.style.height = (~~(zoom * 2000)) + "px"
+	innerContainer.style.width = (~~(zoom * 2000)) + "px"
+
+	innerContainer.style.left = ~~(container.clientWidth / 2 - innerContainer.clientWidth / 2 + zoomOrigin[0] + container.offsetLeft) + "px"
+	innerContainer.style.top = ~~(container.clientHeight / 2 - innerContainer.clientHeight / 2 + zoomOrigin[1] + container.offsetTop) + "px"
+
+}
+
+let atlas = {}
+window.atlas = atlas
+let atlasAll = {}
+window.atlasAll = atlasAll
+
+if (document.location.host !== prodDomain) document.body.dataset.dev = ""
+
+init()
+
+async function init() {
+
+	let mode = "explore"
+
+	const args = window.location.search
+	const params = new URLSearchParams(args)
+	if (args) {
+
+		// Backwards compatibility for old links using "search" id arg
+		if (params.has('id') && params.get('mode') !== 'draw') {
+			const id = params.get('id')
+			params.delete('id')
+			const newLocation = new URL(window.location)
+			newLocation.hash = id
+			newLocation.search = params
+			window.history.replaceState({}, '', newLocation)
+		}
+	}
+
+	const hash = window.location.hash.substring(1)
+	const [, period] = hash.split('/')
+
+	if (period) {
+		const [, targetPeriod, targetVariation] = parsePeriod(period)
+		await updateTime(targetPeriod, targetVariation)
+	} else {
+		await updateTime(currentPeriod, currentVariation)
+	}
+
+	//console.log(document.documentElement.clientWidth, document.documentElement.clientHeight)
+
+	zoomOrigin = [0, 0]
+	applyView()
+
+	let initialPinchDistance = 0
+	let initialPinchZoom = 0
+	let initialPinchZoomOrigin = [0, 0]
+
+	let desiredZoom
+	let zoomAnimationFrame
+
+	document.body.dataset.mode = mode
+
+	initGlobal()
+	initViewGlobal()
+	initExplore()
+
+	document.getElementById("loading").classList.add("d-none")
+
+	document.getElementById("zoomInButton").addEventListener("click", function (e) {
+
+		/*if(zoomAnimationFrame){
+			window.cancelAnimationFrame(zoomAnimationFrame)
+		}*/
+
+		const x = container.clientWidth / 2
+		const y = container.clientHeight / 2
+
+		initialPinchZoomOrigin = [
+			scaleZoomOrigin[0],
+			scaleZoomOrigin[1]
+		]
+
+		initialPinchZoom = zoom
+
+		lastPosition = [x, y]
+		zoom = zoom * 2
+		zoom = Math.max(minZoom, Math.min(maxZoom, zoom))
+
+		applyZoom(x, y, zoom)
+
+	})
+
+	document.getElementById("zoomOutButton").addEventListener("click", function (e) {
+
+		/*if(zoomAnimationFrame){
+			window.cancelAnimationFrame(zoomAnimationFrame)
+		}*/
+
+		const x = container.clientWidth / 2
+		const y = container.clientHeight / 2
+
+		initialPinchZoomOrigin = [
+			scaleZoomOrigin[0],
+			scaleZoomOrigin[1]
+		]
+
+		initialPinchZoom = zoom
+
+		lastPosition = [x, y]
+		zoom = zoom / 2
+		zoom = Math.max(minZoom, Math.min(maxZoom, zoom))
+
+		applyZoom(x, y, zoom)
+	})
+
+	document.getElementById("zoomResetButton").addEventListener("click", function (e) {
+		zoom = 1
+		zoomOrigin = [0, 0]
+		scaleZoomOrigin = [0, 0]
+		applyView()
+	})
+
+	container.addEventListener("dblclick", function (e) {
+		/*if(zoomAnimationFrame){
+			window.cancelAnimationFrame(zoomAnimationFrame)
+		}*/
+
+		const x = e.clientX - container.offsetLeft
+		const y = e.clientY - container.offsetTop
+
+		initialPinchZoomOrigin = [
+			scaleZoomOrigin[0],
+			scaleZoomOrigin[1]
+		]
+
+		initialPinchZoom = zoom
+
+		lastPosition = [x, y]
+
+		if (e.ctrlKey) {
+
+			zoom = zoom / 2
+
+		} else {
+
+			zoom = zoom * 2
+		}
+
+		zoom = Math.max(minZoom, Math.min(maxZoom, zoom))
+		applyZoom(x, y, zoom)
+
+		e.preventDefault()
+	})
+
+
+	container.addEventListener("wheel", function (e) {
+
+		/*if(zoomAnimationFrame){
+			window.cancelAnimationFrame(zoomAnimationFrame)
+		}*/
+
+		const x = e.clientX - container.offsetLeft
+		const y = e.clientY - container.offsetTop
+
+		initialPinchZoomOrigin = [
+			scaleZoomOrigin[0],
+			scaleZoomOrigin[1]
+		]
+
+		initialPinchZoom = zoom
+
+		lastPosition = [x, y]
+
+		// Check if we are zooming by pixels
+		// https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent/deltaMode
+		if (e.deltaMode === 0) {
+			// Scale the pixel delta by the current zoom factor
+			// We want to zoom faster when closer, and slower when further
+			// This creates a smoother experience
+			zoom -= e.deltaY * (0.001 * zoom)
+		} else {
+			if (e.deltaY > 0) {
+
+				zoom = zoom / 2
+
+			} else if (e.deltaY < 0) {
+
+				zoom = zoom * 2
+			}
+		}
+
+		zoom = Math.max(minZoom, Math.min(maxZoom, zoom))
+		applyZoom(x, y, zoom)
+
+		e.preventDefault()
+	}, { passive: true })
+
+	/*function setDesiredZoom(x, y, target){
+		zoom = (zoom*2 + target)/3
+		//console.log(zoom)
+		if(Math.abs(1 - zoom/target) <= 0.01){
+			zoom = target
+		}
+		applyZoom(x, y, zoom)
+		if(zoom != target){
+			zoomAnimationFrame = window.requestAnimationFrame(function(){
+				setDesiredZoom(x, y, target)
+			})
+		}
+	}*/
+
+	container.addEventListener("mousedown", function (e) {
+		mousedown(e.clientX, e.clientY)
+		e.preventDefault()
+	})
+
+	container.addEventListener("touchstart", function (e) {
+
+		if (e.touches.length == 2) {
+			e.preventDefault()
+		}
+
+		touchstart(e)
+
+	}, { passive: true })
+
+	function mousedown(x, y) {
+		lastPosition = [x, y]
+		dragging = true
+	}
+
+	function touchstart(e) {
+
+		if (e.touches.length == 1) {
+
+			mousedown(e.touches[0].clientX, e.touches[0].clientY)
+
+		} else if (e.touches.length == 2) {
+
+			initialPinchDistance = Math.sqrt(
+				Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2)
+				+ Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2)
+			)
+
+			initialPinchZoom = zoom
+			initialPinchZoomOrigin = [
+				scaleZoomOrigin[0],
+				scaleZoomOrigin[1]
+			]
+
+			mousedown(
+				(e.touches[0].clientX + e.touches[1].clientX) / 2,
+				(e.touches[0].clientY + e.touches[1].clientY) / 2
+			)
+
+		}
+
+	}
+
+	window.addEventListener("mousemove", function (e) {
+		mousemove(e.clientX, e.clientY)
+		if (dragging) {
+			e.preventDefault()
+		}
+	})
+	window.addEventListener("touchmove", function (e) {
+
+		if (e.touches.length == 2 || e.scale > 1) {
+			e.preventDefault()
+		}
+
+		touchmove(e)
+
+	},
+		{ passive: false }
+	)
+
+	function mousemove(x, y) {
+		if (dragging) {
+			container.style.cursor = "move"
+
+			const deltaX = x - lastPosition[0]
+			const deltaY = y - lastPosition[1]
+			lastPosition = [x, y]
+
+			zoomOrigin[0] += deltaX
+			zoomOrigin[1] += deltaY
+
+			scaleZoomOrigin[0] += deltaX / zoom
+			scaleZoomOrigin[1] += deltaY / zoom
+
+			previousZoomOrigin = [zoomOrigin[0], zoomOrigin[1]]
+			previousScaleZoomOrigin = [scaleZoomOrigin[0], scaleZoomOrigin[1]]
+
+				applyView()
+		}
+	}
+
+	function touchmove(e) {
+
+
+		if (e.touches.length == 1) {
+
+			mousemove(e.touches[0].clientX, e.touches[0].clientY)
+
+		} else if (e.touches.length == 2) {
+
+			const newPinchDistance = Math.sqrt(
+				Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2)
+				+ Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2)
+			)
+
+			zoom = initialPinchZoom * newPinchDistance / initialPinchDistance
+
+			const x = (e.touches[0].clientX + e.touches[1].clientX) / 2 - container.offsetLeft
+			const y = (e.touches[0].clientY + e.touches[1].clientY) / 2 - container.offsetTop
+
+			applyZoom(x, y, zoom)
+
+		}
+
+	}
+
+	function applyZoom(x, y, zoom) {
+
+		const deltaX = x - lastPosition[0]
+		const deltaY = y - lastPosition[1]
+
+		const pinchTranslateX = (x - container.clientWidth / 2 - deltaX)
+		const pinchTranslateY = (y - container.clientHeight / 2 - deltaY)
+
+		scaleZoomOrigin[0] = initialPinchZoomOrigin[0] + deltaX / zoom + pinchTranslateX / zoom - pinchTranslateX / initialPinchZoom
+		scaleZoomOrigin[1] = initialPinchZoomOrigin[1] + deltaY / zoom + pinchTranslateY / zoom - pinchTranslateY / initialPinchZoom
+
+		zoomOrigin[0] = scaleZoomOrigin[0] * zoom
+		zoomOrigin[1] = scaleZoomOrigin[1] * zoom
+
+		applyView()
+	}
+
+	window.addEventListener("mouseup", function (e) {
+		if (hovered.length > 0) {
+			container.style.cursor = "pointer"
+		} else {
+			container.style.cursor = "default"
+		}
+		if (dragging) {
+			e.preventDefault()
+		}
+		mouseup(e.clientX, e.clientY)
+	})
+	window.addEventListener("touchend", touchend)
+
+	function mouseup(x, y) {
+		if (dragging) {
+			dragging = false
+		}
+	}
+
+	function touchend(e) {
+
+		if (e.touches.length == 0) {
+
+			mouseup()
+
+		} else if (e.touches.length == 1) {
+			initialPinchZoom = zoom
+			lastPosition = [e.touches[0].clientX, e.touches[0].clientY]
+		}
+
+	}
+
+	window.addEventListener("resize", function () {
+		//console.log(document.documentElement.clientWidth, document.documentElement.clientHeight)
+
+		applyView()
+	})
+
+	document.body.dataset.initDone = ''
+
+}
---
+++ web/_js/view.js
@@ -0,0 +1,347 @@
+/*
+	========================================================================
+	The 2022 /r/place Atlas
+
+	An atlas of Reddit's 2022 /r/place, with information to each
+	artwork	of the canvas provided by the community.
+
+	Copyright (c) 2017 Roland Rytz <roland@draemm.li>
+	Copyright (c) 2022 Place Atlas contributors
+
+	Licensed under the GNU Affero General Public License Version 3
+	https://place-atlas.stefanocoding.me/license.txt
+	========================================================================
+*/
+
+let hovered = []
+
+const backgroundCanvas = document.createElement("canvas")
+backgroundCanvas.width = 2000
+backgroundCanvas.height = 2000
+const backgroundContext = backgroundCanvas.getContext("2d")
+
+const wrapper = document.getElementById("wrapper")
+
+const offcanvasList = document.getElementById("offcanvasList")
+
+const objectsListOverflowNotice = document.getElementById("objectsListOverflowNotice")
+
+const entriesList = document.getElementById("entriesList")
+
+let lastPos = [0, 0]
+
+let fixed = false; // Fix hovered items in place, so that clicking on links is possible
+
+offcanvasList.addEventListener('show.bs.offcanvas', function (e) {
+	wrapper.classList.remove('listHidden')
+	wrapper.classList.add('listTransitioning')
+	applyView()
+})
+
+offcanvasList.addEventListener('shown.bs.offcanvas', function (e) {
+	entriesListShown = true
+	wrapper.classList.remove('listTransitioning')
+	updateHovering(e)
+	applyView()
+	render()
+})
+
+offcanvasList.addEventListener('hide.bs.offcanvas', function () {
+	wrapper.classList.add('listHidden')
+	wrapper.classList.add('listTransitioning')
+	applyView()
+})
+
+offcanvasList.addEventListener('hidden.bs.offcanvas', function (e) {
+	entriesListShown = false
+	wrapper.classList.remove('listTransitioning')
+	updateHovering(e)
+	applyView()
+	render()
+})
+
+function toggleFixed(e, tapped) {
+	if (!fixed && hovered.length == 0) {
+		entriesList.classList.remove("disableHover")
+		return 0
+	}
+	fixed = !fixed
+	if (!fixed) {
+		updateHovering(e, tapped)
+		render()
+	}
+	entriesList.classList.add("disableHover")
+	objectsListOverflowNotice.classList.add("d-none")
+}
+
+window.addEventListener("resize", function (e) {
+	//console.log(document.documentElement.clientWidth, document.documentElement.clientHeight)
+
+	// Legacy code
+	let viewportWidth = document.documentElement.clientWidth
+
+	if (document.documentElement.clientWidth > 2000 && viewportWidth <= 2000) {
+		entriesListShown = true
+		wrapper.classList.remove("listHidden")
+	}
+
+	if (document.documentElement.clientWidth < 2000 && viewportWidth >= 2000) {
+		entriesListShown = false
+		wrapper.classList.add("listHidden")
+	}
+	updateHovering(e)
+
+	viewportWidth = document.documentElement.clientWidth
+
+	applyView()
+	render()
+
+})
+
+function renderBackground(atlas) {
+
+	backgroundContext.clearRect(0, 0, canvas.width, canvas.height)
+
+	//backgroundCanvas.width = 1000 * zoom
+	//backgroundCanvas.height = 1000 * zoom
+
+	//backgroundContext.lineWidth = zoom
+
+	backgroundContext.fillStyle = "rgba(0, 0, 0, 0.6)"
+	backgroundContext.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height)
+
+	for (let i = 0; i < atlas.length; i++) {
+
+		const path = atlas[i].path
+
+		backgroundContext.beginPath()
+
+		if (path[0]) {
+			//backgroundContext.moveTo(path[0][0]*zoom, path[0][1]*zoom)
+			backgroundContext.moveTo(path[0][0], path[0][1])
+		}
+
+		for (let p = 1; p < path.length; p++) {
+			//backgroundContext.lineTo(path[p][0]*zoom, path[p][1]*zoom)
+			backgroundContext.lineTo(path[p][0], path[p][1])
+		}
+
+		backgroundContext.closePath()
+
+		let bgStrokeStyle
+		switch (atlas[i].diff) {
+			case "add":
+				bgStrokeStyle = "rgba(0, 255, 0, 1)"
+				backgroundContext.lineWidth = 2
+				break
+			case "edit":
+				bgStrokeStyle = "rgba(255, 255, 0, 1)"
+				backgroundContext.lineWidth = 2
+				break
+			case "delete":
+				bgStrokeStyle = "rgba(255, 0, 0, 1)"
+				backgroundContext.lineWidth = 2
+				break
+			default:
+				bgStrokeStyle = "rgba(255, 255, 255, 0.8)"
+				break
+		}
+		backgroundContext.strokeStyle = bgStrokeStyle
+		backgroundContext.stroke()
+		backgroundContext.lineWidth = 1
+	}
+}
+
+async function render() {
+
+	context.clearRect(0, 0, canvas.width, canvas.height)
+
+	//canvas.width = 1000*zoom
+	//canvas.height = 1000*zoom
+
+	context.globalCompositeOperation = "source-over"
+	context.clearRect(0, 0, canvas.width, canvas.height)
+
+	if (hovered.length > 0) {
+		container.style.cursor = "pointer"
+	} else {
+		container.style.cursor = "default"
+	}
+
+
+	for (let i = 0; i < hovered.length; i++) {
+
+
+		const path = hovered[i].path
+
+		context.beginPath()
+
+		if (path[0]) {
+			//context.moveTo(path[0][0]*zoom, path[0][1]*zoom)
+			context.moveTo(path[0][0], path[0][1])
+		}
+
+		for (let p = 1; p < path.length; p++) {
+			//context.lineTo(path[p][0]*zoom, path[p][1]*zoom)
+			context.lineTo(path[p][0], path[p][1])
+		}
+
+		context.closePath()
+
+		context.globalCompositeOperation = "source-over"
+
+		context.fillStyle = "rgba(0, 0, 0, 1)"
+		context.fill()
+	}
+
+	context.globalCompositeOperation = "source-out"
+	context.drawImage(backgroundCanvas, 0, 0)
+
+	if (hovered.length === 1 && hovered[0].path.length && hovered[0].overrideImage) {
+		const undisputableHovered = hovered[0]
+		// Find the left-topmost point of all the paths
+		const entryPosition = getPositionOfEntry(undisputableHovered)
+		if (entryPosition) {
+			const [startX, startY] = entryPosition
+			const overrideImage = new Image()
+			const loadingPromise = new Promise((res, rej) => {
+				overrideImage.onerror = rej
+				overrideImage.onload = res
+			})
+			overrideImage.src = "imageOverrides/" + undisputableHovered.overrideImage
+			try {
+				await loadingPromise
+				context.globalCompositeOperation = "source-over"
+				context.drawImage(overrideImage, startX, startY)
+			} catch (ex) {
+				console.error("Cannot override image.", ex)
+			}
+		}
+	}
+
+	for (let i = 0; i < hovered.length; i++) {
+
+		const path = hovered[i].path
+
+		context.beginPath()
+
+		if (path[0]) {
+			//context.moveTo(path[0][0]*zoom, path[0][1]*zoom)
+			context.moveTo(path[0][0], path[0][1])
+		}
+
+		for (let p = 1; p < path.length; p++) {
+			//context.lineTo(path[p][0]*zoom, path[p][1]*zoom)
+			context.lineTo(path[p][0], path[p][1])
+		}
+
+		context.closePath()
+
+		context.globalCompositeOperation = "source-over"
+
+		let hoverStrokeStyle
+		switch (hovered[i].diff) {
+			case "add":
+				hoverStrokeStyle = "rgba(0, 155, 0, 1)"
+				break
+			case "edit":
+				hoverStrokeStyle = "rgba(155, 155, 0, 1)"
+				break
+			default:
+				hoverStrokeStyle = "rgba(0, 0, 0, 1)"
+				break
+		}
+		context.strokeStyle = hoverStrokeStyle
+		//context.lineWidth = zoom
+		context.stroke()
+	}
+
+}
+
+function initExplore() {
+
+	window.updateHovering = updateHovering
+	window.render = function () { }
+
+	function updateHovering(e, tapped) {
+		if (!dragging && (!fixed || tapped)) {
+			const pos = [
+				(e.clientX - (container.clientWidth / 2 - innerContainer.clientWidth / 2 + zoomOrigin[0] + container.offsetLeft)) / zoom
+				, (e.clientY - (container.clientHeight / 2 - innerContainer.clientHeight / 2 + zoomOrigin[1] + container.offsetTop)) / zoom
+			]
+			const coords_p = document.getElementById("coords_p")
+			// Displays coordinates as zero instead of NaN
+			if (isNaN(pos[0]) == true) {
+				coords_p.textContent = "0, 0"
+			} else {
+				coords_p.textContent = Math.ceil(pos[0]) + ", " + Math.ceil(pos[1])
+			}
+		}
+	}
+
+	renderBackground(atlas)
+
+	applyView()
+
+}
+
+function initGlobal() {
+	container.addEventListener("mousemove", function (e) {
+		if (e.sourceCapabilities) {
+			if (!e.sourceCapabilities.firesTouchEvents) {
+				updateHovering(e)
+			}
+		} else {
+			updateHovering(e)
+		}
+	})
+}
+
+function initViewGlobal() {
+	container.addEventListener("mousedown", function (e) {
+		lastPos = [
+			e.clientX
+			, e.clientY
+		]
+	})
+
+	container.addEventListener("touchstart", function (e) {
+		if (e.touches.length == 1) {
+			lastPos = [
+				e.touches[0].clientX
+				, e.touches[0].clientY
+			]
+		}
+	}, { passive: true })
+
+	container.addEventListener("mouseup", function (e) {
+		if (Math.abs(lastPos[0] - e.clientX) + Math.abs(lastPos[1] - e.clientY) <= 4) {
+			toggleFixed(e)
+		}
+	})
+
+	container.addEventListener("touchend", function (e) {
+		e.preventDefault()
+
+		//console.log(e)
+		//console.log(e.changedTouches[0].clientX)
+		if (e.changedTouches.length == 1) {
+			e = e.changedTouches[0]
+			//console.log(lastPos[0] - e.clientX)
+			if (Math.abs(lastPos[0] - e.clientX) + Math.abs(lastPos[1] - e.clientY) <= 4) {
+				//console.log("Foo!!")
+				dragging = false
+				fixed = false
+				setTimeout(
+					function () {
+						updateHovering(e, true)
+					}
+					, 10)
+			}
+		}
+	})
+
+	if (window.location.hash) { // both "/" and just "/#" will be an empty hash string
+		highlightEntryFromUrl()
+	}
+}
